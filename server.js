8db080040184785594149891d3fef147a21894a5
jcprice12 <john.c.price12@gmail.com>
2020-03-07 21:11:29 -0500

    added express proxy server

---
+++ server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const port = process.env.PORT || 3000;
+const proxyTarget = process.env.PROXY_TARGET || 'http://localhost:8080';
+const staticPath = path.join(__dirname, 'dist', 'pet-adoption-angular');
+
+const app = express();
+
+app.use('/api', createProxyMiddleware({ 
+    target: proxyTarget,
+    secure: false,
+    pathRewrite: {
+        '^/api': ''
+    }
+}));
+app.use(express.static(staticPath));
+app.get("*", function(__req, res){
+    res.sendFile(path.join(staticPath, 'index.html'));
+});
+
+app.listen(port, function(){
+    console.log("Server listening on port " + port);
+});
