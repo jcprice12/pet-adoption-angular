6d722af07a61fa6425c5c8052a8c72ba5eabf761
jcprice12 <john.c.price12@gmail.com>
2020-03-22 11:37:04 -0400

    added a header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,10 +15,12 @@ import { CatsComponent } from './components/cats/cats.component';
 import { DogCardComponent } from './components/dog-card/dog-card.component';
 import { DogsComponent } from './components/dogs/dogs.component';
 import { FishComponent } from './components/fish/fish.component';
+import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { RegisterComponent } from './components/register/register.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -29,6 +33,7 @@ import { RegisterComponent } from './components/register/register.component';
     LoginComponent,
     DogCardComponent,
     RegisterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -39,7 +44,10 @@ import { RegisterComponent } from './components/register/register.component';
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
+    MatIconModule,
     MatInputModule,
+    MatToolbarModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent],
